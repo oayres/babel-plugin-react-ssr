46afc3ed45d504d3f83f03e48a15932595231029
David Lindley <david.lindley@vodafone.com>
2020-01-06 15:05:17 +0000

    Fixed recurisve components

--- src/visitor/classDeclaration.js
+++ src/visitor/classDeclaration.js
@@ -4,6 +4,9 @@ const classDeclaration = (babel, path, state) => {
   if (classHasRenderMethod(path)) {
     const { body = [] } = path.node.body || {}
 
+    // Store the display name of the class for recusive checks
+    state.file.set('displayName', path.node.id.name)
+
     body.forEach(propertyOrMethod => {
       if (propertyOrMethod.static && propertyOrMethod.key.name === 'fetchData') {
         state.file.set('hasFetchData', true)
